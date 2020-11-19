84f5e692e2d61e233243cdc2e6412f8bc82900b5
saglamorhan61 <saglamorhan.61@gmail.com>
2020-11-19 19:54:15 +0300

    Yer ekle Menu eklendi

--- app/src/main/java/com/saglamorhan/seyahatkitabim/GezilenYerlerActivity.kt
+++ app/src/main/java/com/saglamorhan/seyahatkitabim/GezilenYerlerActivity.kt
@@ -1,11 +1,33 @@
 package com.saglamorhan.seyahatkitabim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class GezilenYerlerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gezilen_yerler)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.yer_ekle,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.yer_ekle_item){
+
+            val intent = Intent(this,MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
